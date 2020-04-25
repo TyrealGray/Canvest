a7798d87d48134fd1f27b533c9c9c52fef234104
Tyreal Gray <tyrealgray@ymail.com>
2020-04-25 23:27:32 +0100

    adding more config and example code

--- packages/canvest-cli/src/index.js
+++ packages/canvest-cli/src/index.js
@@ -5,4 +5,6 @@ const open = require('open');
 (async () => {
 	// Opens the URL in the default browser.
 	await open('https://github.com');
+
+
 })();
---
+++ packages/canvest-core/src/index.js
@@ -0,0 +1,25 @@
+export const snapshot = (canvas, clone) => {
+	try {
+		if (clone) {
+			clone.width = canvas.width;
+			clone.height = canvas.height;
+
+			const canvasCtx = canvas.getContext('2d');
+			const cloneCtx = clone.getContext('2d');
+
+			if (!canvasCtx) {
+				cloneCtx.drawImage(canvas, 0, 0);
+			} else {
+				cloneCtx.putImageData(
+					canvasCtx.getImageData(0, 0, canvas.width, canvas.height),
+					0,
+					0,
+				);
+			}
+
+			return clone.toDataURL('image/png');
+		}
+	} catch (e) {
+		console.error(`snapshot canvas failed, ${e}`);
+	}
+};

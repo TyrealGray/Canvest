8c63db21c671c8c0f0b3e99819d51076d8926f25
Tyreal Gray <tyrealgray@ymail.com>
2020-05-01 20:37:04 +0100

    improve snapshot function, snapshot more times and compare quickly to avoid timeout render issue. close #6

--- packages/canvest-cli/canvest/test.canvest.js
+++
@@ -1,5 +0,0 @@
-describe('sum', () => {
-	it('should return sum of arguments', () => {
-		expect(3).to.equal(3);
-	});
-});
--- packages/canvest-core/src/index.js
+++ packages/canvest-core/src/index.js
@@ -1,9 +1,7 @@
-/**
- * compare two binary arrays for equality
- * @param {(ArrayBuffer|ArrayBufferView)} a
- * @param {(ArrayBuffer|ArrayBufferView)} b
- */
-export function isPixelEqual(a, b) {
+export function isPixelEqual(snapshotA, snapshotB) {
+
+	let a = snapshotA.buffer, b = snapshotB.buffer;
+
 	if (a instanceof ArrayBuffer) a = new Uint8Array(a, 0);
 	if (b instanceof ArrayBuffer) b = new Uint8Array(b, 0);
 	if (a.byteLength !== b.byteLength) return false;
@@ -19,11 +17,13 @@ function equal8(a, b) {
 	const ub = new Uint8Array(b.buffer, b.byteOffset, b.byteLength);
 	return compare(ua, ub);
 }
+
 function equal16(a, b) {
 	const ua = new Uint16Array(a.buffer, a.byteOffset, a.byteLength / 2);
 	const ub = new Uint16Array(b.buffer, b.byteOffset, b.byteLength / 2);
 	return compare(ua, ub);
 }
+
 function equal32(a, b) {
 	const ua = new Uint32Array(a.buffer, a.byteOffset, a.byteLength / 4);
 	const ub = new Uint32Array(b.buffer, b.byteOffset, b.byteLength / 4);
@@ -45,32 +45,74 @@ function aligned32(a) {
 	return (a.byteOffset % 4 === 0) && (a.byteLength % 4 === 0);
 }
 
+const captureImage = (canvas, cloneCanvas) => {
+	try {
+		const canvasCtx = canvas.getContext('2d');
+		const cloneCtx = cloneCanvas.getContext('2d');
+
+		if (!canvasCtx) {
+			cloneCtx.drawImage(canvas, 0, 0);
+		} else {
+			cloneCtx.putImageData(
+				canvasCtx.getImageData(0, 0, canvas.width, canvas.height),
+				0,
+				0,
+			);
+		}
+
+		return {
+			buffer: cloneCtx.getImageData(0, 0, canvas.width, canvas.height).data.buffer,
+			dataURL: cloneCanvas.toDataURL()
+		};
+	} catch (e) {
+		throw e;
+	}
+};
+
 export const snapshot = (canvas) => {
-	return new Promise((resolve, reject)=>{
-		setTimeout(()=> {
+
+	return new Promise((resolve, reject) => {
+
+		const captures = [];
+
+		const tempCanvas = document.createElement('canvas');
+
+		tempCanvas.width = canvas.width;
+		tempCanvas.height = canvas.height;
+
+		const timeout = (parseInt(canvas.width / 1000.0) + parseInt(canvas.height / 1000.0)) * 50 + 100;
+
+		const stableCapture = (canvas, cloneCanvas) => {
 			try {
-				const tempCanvas = document.createElement('canvas');
-				tempCanvas.width = canvas.width;
-				tempCanvas.height = canvas.height;
 
-				const canvasCtx = canvas.getContext('2d');
-				const cloneCtx = tempCanvas.getContext('2d');
+				const capture = captureImage(canvas, cloneCanvas);
 
-				if (!canvasCtx) {
-					cloneCtx.drawImage(canvas, 0, 0);
+				if (captures.length < 3) {
+					captures.push(capture);
 				} else {
-					cloneCtx.putImageData(
-						canvasCtx.getImageData(0, 0, canvas.width, canvas.height),
-						0,
-						0,
-					);
+					if (isPixelEqual(captures[0], captures[1])
+						&& isPixelEqual(captures[1], captures[2])
+						&& isPixelEqual(captures[2], capture)) {
+
+						return resolve({...capture});
+					} else {
+						console.warn(`snapshot not stable redo capture`);
+						captures.shift();
+						captures.push(capture);
+					}
 				}
 
-				resolve(cloneCtx.getImageData(0, 0, canvas.width, canvas.height).data.buffer);
+				setTimeout(() => {
+					stableCapture(canvas, cloneCanvas);
+				}, timeout);
 			} catch (e) {
 				reject(`snapshot canvas failed, ${e}`);
 			}
-		},100);
+		};
+
+		setTimeout(() => {
+			stableCapture(canvas, tempCanvas);
+		}, 100);
 	});
 
 };

524ecd9b75c522f66dd0650853d1896a08472ab3
Tyreal Gray <tyrealgray@ymail.com>
2020-05-02 19:17:46 +0100

    make capture object api more testing friendly

--- packages/canvest-cli/index.js
+++ packages/canvest-cli/index.js
@@ -6,6 +6,7 @@ const processRunNPM = require('./src/processUtil').processRunNPM;
 (async () => {
 	if (argv.ts) {
 		await processRunNPM(['run','tsc', './canvest/*.ts'], process.cwd());
+		console.log('convert to js testing files ready.')
 	}
 	await processRunNode(['./node_modules/@canvest/canvest-dev-server/bin/canvest-dev-server.js'], process.cwd());
 })();
--- packages/canvest-dev-server/script/init.js
+++ packages/canvest-dev-server/script/init.js
@@ -1,2 +1,25 @@
 import { snapshot } from '@canvest/canvest-core';
-window.snapshot = snapshot;
+
+window.snapshot = (canvasView) => {
+	return new Promise((resolve => {
+		snapshot(canvasView).then((capture) => {
+			capture.isEqual = (otherCapture) => {
+				expect(capture.equal(otherCapture)).to.equal(true);
+			};
+
+			capture.notEqual = (otherCapture) => {
+				expect(capture.equal(otherCapture)).to.equal(false);
+			};
+
+			capture.isContain = (otherCapture) => {
+				// expect(capture.contain(otherCapture)).to.equal(true);
+			};
+
+			capture.notContain = (otherCapture) => {
+				// expect(capture.contain(otherCapture)).to.equal(false);
+			};
+
+			resolve(capture);
+		});
+	}));
+};

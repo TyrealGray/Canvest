38056db6d56f51f32164eb4fe3adbf73b8938298
Tyreal Gray <tyrealgray@ymail.com>
2020-05-03 13:47:17 +0100

    trying to fix pixelmatch is not a function error

--- packages/canvest-cli/canvest/ts.canvest.ts
+++ packages/canvest-cli/canvest/ts.canvest.ts
@@ -1,7 +1,7 @@
 describe('TS', function () {
 	it('should pass', function() {
 		const a: number = 1;
-		const b: number = 3;
-		expect(a + 2).to.equal(b);
+		const b: number = 2;
+		expect(a + 1).to.equal(b);
 	});
 });
